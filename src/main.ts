8db51fb2837836e05ae0d55ea1d375271419d48f
Adam Cooper <adam@threads.com>
2021-05-11 13:29:37 -0700

    Test

--- src/main.ts
+++ src/main.ts
@@ -3,50 +3,54 @@ import * as github from "@actions/github";
 
 async function run() {
   try {
+
+    const hasProjects = github.context!.payload!.pull_request!.has_projects
     const milestone = github.context!.payload!.pull_request!.milestone;
     const labels = github.context!.payload!.pull_request!.labels;
 
+    console.log(github.context!.payload!.pull_request);
+
     enforceMilestone(milestone);
-    enforceLabels(labels);
+    enforceProjects(hasProjects);
+    enforcePlatformLabels(labels);
+    enforceTypeLabels(labels)
+
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 function enforceMilestone(milestone) {
-  if (!milestone) {
-    const requiredLabelsAnyDescription = getInputString(
-      "REQUIRED_MILESTONE_DESCRIPTION",
-      `Please add a milestone to this PR`
-    );
-    core.setFailed(requiredLabelsAnyDescription);
+  const enabled: boolean = core.getBooleanInput("CHECK_FOR_MILESTONE");
+  if (enabled && !milestone) {
+    core.setFailed('Please add a milestone to this PR');
   }
 }
 
-function enforceLabels(labels) {
-  const requiredLabels: string[] = getInputArray("REQUIRED_LABELS");
-  if (
-    requiredLabels.length > 0 &&
-    !requiredLabels.some((requiredLabel) =>
-      labels.find((l) => l.name === requiredLabel)
-    )
-  ) {
-    const requiredLabelsAnyDescription = getInputString(
-      "REQUIRED_LABELS_DESCRIPTION",
-      `Please add labels to this PR: ${requiredLabels}`
-    );
-    core.setFailed(requiredLabelsAnyDescription);
+function enforceProjects(hasProjects) {
+  const enabled: boolean = core.getBooleanInput("CHECK_FOR_PROJECTS");
+  if (enabled && !hasProjects) {
+    core.setFailed('Please add a milestone to this PR');
+  }
+}
+
+function enforcePlatformLabels(labels) {
+  const platformLabels: string[] = getInputArray("PLATFORM_LABELS");
+  if (platformLabels.length > 0 && !platformLabels.some((requiredLabel) => labels.find((l) => l.name === requiredLabel))) {
+    core.setFailed(`Please add a platform label to this PR: ${platformLabels}`);
+  }
+}
+
+function enforceTypeLabels(labels) {
+  const typeLabels: string[] = getInputArray("TYPE_LABELS");
+  if (typeLabels.length > 0 && !typeLabels.some((requiredLabel) => labels.find((l) => l.name === requiredLabel))) {
+    core.setFailed(`Please add a type label to this PR: ${typeLabels}`);
   }
 }
 
 function getInputArray(name): string[] {
   const rawInput = core.getInput(name, { required: false });
   return rawInput !== "" ? rawInput.split(",") : [];
-}
-
-function getInputString(name, defaultValue): string {
-  const rawInput = core.getInput(name, { required: false });
-  return rawInput !== "" ? rawInput : defaultValue;
 }
 
 run();
